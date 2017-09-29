1af8a21f618982e8ba09a1a8506f57134e7c9202
PavlinCPetkov <pavlinpetkov33@gmail.com>
2017-09-29 08:41:51 +0300

    Add files via upload

---
+++ BasicAndIntroLab/AddTwoNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int result = firstNumber + secondNumber;
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {result}");
+        }
+    }
+}
